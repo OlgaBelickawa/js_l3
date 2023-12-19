abaa07ea933c1096bbdba0d77e8ac960a5403c2e
OlgaBelickawa <145890221+OlgaBelickawa@users.noreply.github.com>
2023-12-19 10:47:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// 1. string - строка
+const favoriteIceCream = "Bahroma";
+console.log(favoriteIceCream);
+
+// 2. number - число
+const numberIceCream = "3";
+console.log(numberIceCream);
+
+// 3. boolean - ложь или истина
+const isCouldIceCream = true;
+console.log(isCouldIceCream);
+
+// 4. null - пустое илинеизвестное
+let Ice;
+console.log(Ice);
+
+// 5. undefained - не назначенное значение
+const user = {
+    name: "Olga",
+    favoriteIceCream: "Bahroma",
+    numberIceCream: "3",
+};
+console.log(user);
